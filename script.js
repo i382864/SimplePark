3af8155d55957391245738950c7cb0f22c757d66
Topboycoder <diyar.toprak@hotmail.com>
2024-12-18 10:32:25 +0100

    added menu

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-document.getElementById('menu-button').addEventListener('click', () => {
-  const dropdown = document.getElementById('dropdown');
-  dropdown.classList.toggle('hidden');
-});
+  const burgerMenu = document.querySelector('.menu-button'); 
+  const closeButton = document.querySelector('#closeButton'); 
+  const overlay = document.querySelector('#overlay');
+
+  burgerMenu.addEventListener('click', () => {
+      overlay.style.display = 'flex';
+  });
+  closeButton.addEventListener('click', () => {
+      overlay.style.display = 'none';
+  });
+  overlay.addEventListener('click', (e) => {
+      if (e.target === overlay) {
+          overlay.style.display = 'none';
+      }
+  });
+  function setActive(buttonId) {
+          
+    document.querySelectorAll('.button').forEach(button => {
+    button.classList.remove('active');
+        });
+
+         
+      document.getElementById(buttonId).classList.add('active');
+      }
